f8def213f86e0380a960eb4ee87d1e369e055756
rmilzi <kannymayo@outlook.com>
2022-12-20 11:08:03 +0000

    Integrated in Tailwind vis PostCSS.

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -5,9 +5,14 @@ function App() {
     const [count, setCount] = useState(0)
 
     return (
-        <button onClick={() => setCount((count) => count + 1)}>
-            count is {count + 10}
-        </button>
+        <div>
+            <button
+                className="px-6 py-2 rounded bg-green-800 hover:bg-green-600 text-white"
+                onClick={() => setCount((count) => count + 1)}
+            >
+                count is {count + 10}
+            </button>
+        </div>
     )
 }
 
