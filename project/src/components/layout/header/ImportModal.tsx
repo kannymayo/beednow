f9aed1cf8b7a0d49fd5b5e7d8a13be1765065d26
rmilzi <kannymayo@outlook.com>
2023-01-30 14:51:35 +0800

    Randomized demo import.
    Editable content to textarea. Refactor.

--- project/src/components/layout/header/ImportModal.tsx
+++ project/src/components/layout/header/ImportModal.tsx
@@ -1,5 +1,6 @@
 import './ImportModal.css'
 import React, { useState, useEffect, useRef, useMemo, useReducer } from 'react'
+import clsx from 'clsx'
 import produce from 'immer'
 
 import { useItemDetailsMultiple, ItemFromAPI } from '../../../api/itemDetails'
@@ -8,13 +9,13 @@ import { readGeneralExport } from '../../../utils/read-export'
 import ImportableItemGroup from './importableItemGroup'
 
 export default function ImportModal() {
-  const [isOpen, setIsOpen] = useState(false)
   const [itemOccurrencesGrouped, itemOccurrencesGroupedDispatch] = useReducer(
     iocGroupedReducer,
     {}
   )
+  const [isOpen, setIsOpen] = useState(false)
   const [importString, setImportString] = useState('')
-  const editableDivRef = useRef<HTMLDivElement>(null)
+  const refTextarea = useRef<HTMLTextAreaElement>(null)
 
   const idList = useMemo(
     () => readGeneralExport(importString) || [],
@@ -30,6 +31,7 @@ export default function ImportModal() {
   const validItemCount =
     Object.entries(itemOccurrencesGrouped).flat(2).length - validItemUniqueCount
 
+  // async query
   const itemDetailsQryRslts = useItemDetailsMultiple(idList)
   const itemOccurrences = useMemo(() => {
     return annotate(createSkeleton(idList), itemDetailsQryRslts)
@@ -49,7 +51,7 @@ export default function ImportModal() {
   ])
 
   var modalTitle = (
-    <>
+    <h3 className="text-lg font-bold">
       Add more items for bidding
       <button
         className="btn btn-secondary btn-xs ml-5"
@@ -57,22 +59,27 @@ export default function ImportModal() {
       >
         Add Demo Data
       </button>
-    </>
+    </h3>
   )
 
+  const importTextareaCls = clsx(
+    'textarea textarea-bordered scrollbar-hide m-1 flex-1 overflow-y-auto border-2 p-1 outline-0'
+  )
   const importTextarea = (
-    <div
-      className="textarea textarea-bordered scrollbar-hide m-1 flex-1 overflow-y-auto border-2 p-1 outline-0"
-      contentEditable
+    <textarea
+      className={importTextareaCls}
       data-placeholder="Paste here your text containing item IDs."
-      onInput={handleInput}
-      onPaste={handlePaste}
-      ref={editableDivRef}
-    ></div>
+      onChange={handleTextareaChange}
+      ref={refTextarea}
+      value={importString}
+    ></textarea>
   )
 
+  const importPreviewCls = clsx(
+    'card min-h-16 flex-1 place-items-stretch gap-2 overflow-y-auto rounded-sm px-2 py-1'
+  )
   const importPreview = (
-    <div className="card min-h-16 flex-1 place-items-stretch gap-2 overflow-y-auto rounded-sm px-2 py-1">
+    <div className={importPreviewCls}>
       {Object.entries(itemOccurrencesGrouped).map((tuple, index) => (
         <ImportableItemGroup
           group={tuple[1] as ItemOccurrence[]}
@@ -84,16 +91,27 @@ export default function ImportModal() {
     </div>
   )
 
-  const importSummary = idList.length > 1 && (
-    <div className="flex place-items-center gap-1">
-      Detected
-      <div className="badge badge-accent">{validItemCount}</div>
-      items of
-      <div className="badge badge-accent">{validItemUniqueCount}</div>
-      types
+  const summaryContainerCls = clsx(
+    'flex flex-grow-0 place-items-center justify-end gap-6'
+  )
+  const importSummaryAndAction = (
+    <div className={summaryContainerCls}>
+      {idList.length > 1 && (
+        <div className="flex place-items-center gap-1">
+          Detected
+          <div className="badge badge-accent">{validItemCount}</div>
+          items of
+          <div className="badge badge-accent">{validItemUniqueCount}</div>
+          types
+        </div>
+      )}
+      <button className="btn btn-primary btn-sm">Import</button>
     </div>
   )
 
+  const modalContainerCls = clsx(
+    'modal-box relative flex min-h-[16rem] max-w-xl flex-col gap-4 md:max-w-2xl lg:max-w-4xl'
+  )
   const _RETURN = (
     <>
       <input
@@ -101,26 +119,20 @@ export default function ImportModal() {
         id="import-modal"
         className="modal-toggle"
         checked={isOpen}
-        onChange={handleChange}
+        onChange={handleModalChange}
       />
       {/* MODEL BACKDROP */}
       <label htmlFor="import-modal" className="modal cursor-pointer">
         {/* MODEL CONTAINER with overwriting ability, so clicking inside won't dismiss?*/}
-        <label
-          className="modal-box relative flex min-h-[16rem] max-w-xl flex-col gap-4 md:max-w-2xl lg:max-w-4xl"
-          htmlFor=""
-        >
-          <h3 className="text-lg font-bold">{modalTitle}</h3>
+        <label className={modalContainerCls} htmlFor="">
+          {modalTitle}
           <div className="flex flex-1 flex-col gap-4">
             <div className="flex max-h-96 w-full flex-1">
               {importTextarea}
               <div className="divider divider-horizontal"></div>
               {importPreview}
             </div>
-            <div className="flex flex-grow-0 place-items-center justify-end gap-6">
-              {importSummary}
-              <button className="btn btn-primary btn-sm">Import</button>
-            </div>
+            {importSummaryAndAction}
           </div>
         </label>
       </label>
@@ -129,44 +141,26 @@ export default function ImportModal() {
 
   return _RETURN
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleModalChange(e: React.ChangeEvent<HTMLInputElement>) {
     setIsOpen(e.target.checked)
-    console.log('called')
     setTimeout(() => setImportString(''), 300)
   }
 
-  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
-    console.log(e)
-    setImportString(e.target.innerText)
-  }
-
-  function handlePaste(e: React.ClipboardEvent<HTMLInputElement>) {
-    // buggy when performing paste at cursor & paste to selection
-    // maybe it's still a better option to go with a textarea
-
-    // pasting uncleaned text would mess up editable div
-    e.preventDefault()
-    const text = e.clipboardData.getData('text/plain')
-
-    if (editableDivRef.current) {
-      editableDivRef.current.innerText =
-        editableDivRef.current.innerText.concat(text)
-      editableDivRef.current.dispatchEvent(
-        new Event('input', { bubbles: true })
-      )
-    }
+  function handleTextareaChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setImportString(e.target.value)
   }
 
   function handleAddDemoData() {
-    if (editableDivRef.current !== null) {
-      editableDivRef.current.innerText += `[
-        //   40273, 40247, 40254, 44577, 40278, 40288, 40627, 40317, 40207, 40065, 40346,
-        //   40636, 40303, 40256, 40258, 40384,
-        // ]`
-      editableDivRef.current.dispatchEvent(
-        new Event('input', { bubbles: true })
-      )
-    }
+    const idPool = [
+      40273, 40247, 40254, 44577, 40278, 40288, 40627, 40317, 40207, 40065,
+      40346, 40636, 40303, 40256, 40258, 40384,
+    ]
+    const idList = Array.from(
+      { length: 15 },
+      () => idPool[Math.floor(Math.random() * idPool.length)]
+    )
+    const text = idList.join(' ')
+    setImportString((str) => str + text)
   }
 }
 
