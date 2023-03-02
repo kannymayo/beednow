2e44186110619ea342c04d50ab2b90b18d995670
rmilzi <kannymayo@outlook.com>
2023-03-02 20:11:05 +0800

    fix: isRoomHost not syncing

--- project/src/layout/aside/WithHeader.tsx
+++ project/src/layout/aside/WithHeader.tsx
@@ -26,7 +26,8 @@ export default function WithHeader() {
     resetOnUnmount: true,
   })
   useEffect(() => {
-    if (user.uid) setIsRoomHost(user.uid === roomInfo?.hostedBy)
+    if (!user.uid || !roomInfo?.hostedBy) return setIsRoomHost(false)
+    setIsRoomHost(user.uid === roomInfo?.hostedBy)
   }, [roomInfo?.hostedBy, user.uid])
 
   // sync roomIdd in url to Atom
