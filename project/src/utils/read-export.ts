a7b78e924cfece6b661fc934ffcab12de4c139ca
rmilzi <kannymayo@outlook.com>
2023-03-05 23:42:44 +0800

    cleanup: ledger import string no longer needed

--- project/src/utils/read-export.ts
+++ project/src/utils/read-export.ts
@@ -1,26 +1,3 @@
-import itemsRaw from '../../mock-data/items-export'
-
-function readWowLedgerExport() {
-  const items = itemsRaw.split('\n')
-  // extract whats between the first set of square brackets
-  items.map((item, index) => {
-    const itemMatch = item.match(/\[(.*?)\]/)
-    if (itemMatch) {
-      items[index] = itemMatch[1]
-    }
-  })
-
-  // extract whats between the first set of brackets
-  items.map((item, index) => {
-    const itemMatch = item.match(/\((.*?)\)/)
-    if (itemMatch) {
-      items[index] = itemMatch[1]
-    }
-  })
-
-  return items
-}
-
 function readGeneralExport(text: string = '') {
   const regex = /\d{5}/g
   const matches = text.match(regex)
@@ -28,4 +5,3 @@ function readGeneralExport(text: string = '') {
 }
 
 export { readGeneralExport }
-export default readWowLedgerExport
