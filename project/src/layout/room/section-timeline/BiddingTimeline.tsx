feb58fdfb9b9f3b6abb4ca6c63ece9aa10fff9f3
liziyang <kannymayo@outlook.com>
2023-03-12 14:03:08 +0800

    fix: slide in animation breaks section sizing

--- project/src/layout/room/section-timeline/BiddingTimeline.tsx
+++ project/src/layout/room/section-timeline/BiddingTimeline.tsx
@@ -1,6 +1,6 @@
 import './BiddingTimeline.css'
 import { useRef, useEffect } from 'react'
-import { toast, ToastContainer } from 'react-toastify'
+import { toast, ToastContainer, Zoom } from 'react-toastify'
 import { useDebounce } from 'ahooks'
 
 import {
@@ -86,6 +86,7 @@ export default function BidHistory() {
         enableMultiContainer
         containerId={'section-timeline'}
         position={toast.POSITION.BOTTOM_CENTER}
+        transition={Zoom}
       />
     </div>
   )
